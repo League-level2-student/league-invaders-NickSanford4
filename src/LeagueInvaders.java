64bc418d9bb278e103f29d7a75fe119f18270075
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-06-12 17:00:59 -0700

    League Invaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,13 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	JFrame frame = new JFrame();
+	final int HEIGHT = 800;
+	final int WIDTH = 500;
+public static void main(String[] args) {
 
+}
+LeagueInvaders(){
+		
+	}
 }
