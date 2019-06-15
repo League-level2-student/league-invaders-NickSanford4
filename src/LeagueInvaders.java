ff41d8f48d20e2a7ab5c8d90a5240d1a46cc8515
League Student <league@AdministorsiMac.attlocal.net>
2019-06-15 16:00:19 -0700

    League Invaders with the GamePanel

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,13 +1,26 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class LeagueInvaders {
 	JFrame frame = new JFrame();
+	GamePanel gamepanel;
 	final int HEIGHT = 800;
 	final int WIDTH = 500;
 public static void main(String[] args) {
-
+LeagueInvaders leagueInvaders = new LeagueInvaders();
+leagueInvaders.setup();
 }
 LeagueInvaders(){
-		
-	}
+		frame = new JFrame();
+		gamepanel = new GamePanel();
+}
+private void setup() {
+	JPanel panel = new JPanel();
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	panel.setVisible(true);
+	frame.setVisible(true);
+}
 }
